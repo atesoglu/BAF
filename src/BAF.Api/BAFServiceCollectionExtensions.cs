94f92f479569bc2e6ef57aadc98b8d50de9a1fb8
Kadir Atesoglu <atesoglu@live.com>
2019-02-11 17:12:02 +0300

    BAF.Api project type change.

---
+++ src/BAF.Api/BAFServiceCollectionExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BAF.Api
+{
+    public static class BAFServiceCollectionExtensions
+    {
+        public static IBAFBuilder AddBAF(this IServiceCollection services)
+        {
+            var builder = new BAFBuilder();
+            builder.App.ConfigureServices();
+
+            return builder;
+        }
+
+        public static IApplicationBuilder UseBAF(this IApplicationBuilder applicationBuilder)
+        {
+            BAF.App.RegisterIocComponents();
+            BAF.App.Configure();
+            BAF.App.Verify();
+
+            return applicationBuilder;
+        }
+    }
+}
