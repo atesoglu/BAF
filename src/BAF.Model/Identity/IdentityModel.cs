78331a1623e54322b238fd8faf03c89c115287db
Kadir Atesoglu <atesoglu@live.com>
2019-02-18 11:47:10 +0300

    Sdk Update 2.1 -> 2.2

--- src/BAF.Model/Identity/IdentityModel.cs
+++ src/BAF.Model/Identity/IdentityModel.cs
@@ -1,7 +1,11 @@
-﻿namespace BAF.Model.Identity
+﻿using System;
+
+namespace BAF.Model.Identity
 {
     public class IdentityModel : IIdentityModel
     {
+        public static IIdentityModel System = new IdentityModel { Identifier = "sys", Name = "System Owner", IpAddress = "127.0.0.1", MachineName = Environment.MachineName };
+
         public string Identifier { get; set; }
         public string Name { get; set; }
         public string IpAddress { get; set; }
